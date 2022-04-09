2c290e472affb51226e04031679d70560320c479
Catherine Tranfield <victoria.tranfield@gmail.com>
2022-04-09 01:35:23 +0100

    admin interface added

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -92,7 +92,8 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

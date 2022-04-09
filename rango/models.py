ce85d93220ceb8c8c6a7db9562da83b34d93f490
Catherine Tranfield <victoria.tranfield@gmail.com>
2022-04-10 00:37:58 +0100

    Models - page and category added

--- rango/models.py
+++ rango/models.py
@@ -5,9 +5,19 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    # views = models.IntegerField(default=0)
+    # likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
+    
+    def add_page(cat, title, url, views=0):
+        p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
+        return p
+
+    def add_cat(name, views=0, likes=0):
+        c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+        return c
 
     def __str__(self):
         return self.name
